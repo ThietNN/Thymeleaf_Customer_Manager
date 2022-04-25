4904a5b273967aa30179f23ad757f76459d4c748
ThietNN <nguyenthiet1225@gmail.com>
2022-04-25 10:14:25 +0700

    new pj

---
+++ src/main/java/ThietNN/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package ThietNN.service;
+
+import ThietNN.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
